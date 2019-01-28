f533c97a693b3cfd34ef80ce854e7d33f0483b96
njnisarg <NJnisarg@gmail.com>
2019-01-29 00:50:42 +0530

    Added the skeleton for search and apis in general

---
+++ controllers/searchController.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'get': async (req,res) => {
+      let searchString = req.query.searchString;
+      let city = req.query.city;
+      let org = req.query.org;
+
+      if(searchString===null || searchString===undefined){
+          // Throw error
+
+      }
+      else{
+
+      }
+
+  }
+};
