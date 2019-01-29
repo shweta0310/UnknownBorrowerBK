b7241301d3a32efd3dfaa38e25cefe533aef50cb
njnisarg <NJnisarg@gmail.com>
2019-01-30 01:16:50 +0530

    Fixed the issues hopefully

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-// const controller = require('./loginController');
+const loginController = require('./loginController');
 
-/* GET users listing. */
-// router.post('/login', loginController.authenticateUser);
+ // GET users listing.
+router.post('/login', loginController.authenticateUser);
 
 module.exports = router;
 
