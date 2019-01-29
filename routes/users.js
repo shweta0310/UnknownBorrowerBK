2f1e11f2aad76319bb31f633d95e7376ea279248
siddhartha15 <siddhartha.15.manju@gmail.com>
2019-01-29 23:47:19 +0530

    adding login auth backend

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
+const controller = require('./loginController');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/login', loginController.authenticateUser);
 
 module.exports = router;
+
+
