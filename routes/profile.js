cb45c73fa6566fa8ef2a002ad20fac6089f6e1ed
yash-m-agarwal <yashmagarwal786@gmail.com>
2019-02-02 23:03:41 +0530

    Created the profile creation and updation

--- routes/profile.js
+++ routes/profile.js
@@ -36,7 +36,7 @@ profileRouter.get('/:id', async (req, res, next) =>  {
 
 /* PUT request for adding wallet money*/
 
-profileRouter.put('/addMoney/:id', async (req,res,next) => {
+profileRouter.put('/addMoney/:id', async (req, res, next) => {
     let userProfile = await profile.findOne({
         where:{
             userId:req.params.id
@@ -46,7 +46,67 @@ profileRouter.put('/addMoney/:id', async (req,res,next) => {
         console.log('Rows updated' + count)
     });
 
-    response(res, null, "Done man!",null, 201);
+    response(res, null, "Done man!",null, 202);
+});
+
+/* PUT request to update the Profile */
+
+profileRouter.put('/editProfile/:id', async (req, res, next) => {
+   profile.update({
+       'name' : req.body.name,
+       'city' : req.body.city,
+       'state' : req.body.state,
+       'org' : req.body.org,
+       'country' : req.body.country,
+       'age' : req.body.age,
+       'gender' : req.body.gender,
+       'occupation' : req.body.occupation}, { where : { userId: req.params.id }}).then(count => {
+           console.log('Rows updated' + count)
+   });
+
+   response(res, null, "Profile updated", null, 202);
+
+});
+
+/* POST request to create the Profile */
+
+profileRouter.post('/createProfile', async (req, res, next) => {
+
+    const {
+        name,
+        city,
+        state,
+        org,
+        country,
+        contactNum,
+        emailId,
+        ratings,
+        age,
+        gender,
+        occupation,
+        balance
+    } = req.body;
+
+   profile.create({
+       name : name,
+       city : city,
+       state : state,
+       org : org,
+       country : country,
+       contactNum : contactNum,
+       emailId : emailId,
+       ratings : ratings,
+       age : age,
+       gender : gender,
+       occupation : occupation,
+       balance : balance})
+       .then(user => {
+           response(res,null,user,null,201);
+   })
+    .catch(err => {
+        response(res,null, err,null,500);
+    });
+
 });
 
 module.exports = profileRouter;
